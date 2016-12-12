f40a9243886d01f7c4703cc0263a8e81f668f418
Apurva Patkeshwar <apurva.p@hotmail.com>
2016-12-12 12:00:53 -0500

    Moving SearchEngine and SearchServlet to correct src location

--- src/edu/nyu/cs/cs2580/SearchEngine.java
+++ src/edu/nyu/cs/cs2580/SearchEngine.java
@@ -1,92 +1,28 @@
-package edu.nyu.cs.cs2580;
+package project;
 
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.Serializable;
 import java.net.InetSocketAddress;
-import java.security.NoSuchAlgorithmException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.Executors;
 
 import com.sun.net.httpserver.HttpServer;
 
-/**
- * This is the main entry class for the Search Engine.
- *
- * Usage (must be running from the parent directory of src):
- *  0) Compiling
- *   javac src/edu/nyu/cs/cs2580/*.java
- *  1) Mining
- *   java -cp src edu.nyu.cs.cs2580.SearchEngine \
- *     --mode=mining --options=conf/engine.conf
- *  2) Indexing
- *   java -cp src edu.nyu.cs.cs2580.SearchEngine \
- *     --mode=index --options=conf/engine.conf
- *  3) Serving
- *   java -cp src -Xmx256m edu.nyu.cs.cs2580.SearchEngine \
- *     --mode=serve --port=[port] --options=conf/engine.conf
- *  4) Searching
- *   http://localhost:[port]/search?query=web&ranker=fullscan
- *
- * @CS2580:
- * You must ensure your program runs with maximum heap memory size -Xmx512m.
- * You must use a port number 258XX, where XX is your group number.
- *
- * Students do not need to change this class except to add server options.
- *
- * @author congyu
- * @author fdiaz
- */
 public class SearchEngine {
 
-  /**
-   * Stores all the options and configurations used in our search engine.
-   * For simplicity, all options are publicly accessible.
-   */
-
-  public static class Options implements Serializable {
+  public static class Options {
     // The parent path where the corpus resides.
-    // HW1: We have only one file, corpus.csv.
-    // HW2/HW3: We have a partial Wikipedia dump.
     public String _corpusPrefix = null;
-
-    // The parent path where the log date reside.
-    // HW1/HW2: n/a
-    // HW3: We have a partial Wikipedia visit log dump.
-    public String _logPrefix = null;
-
+    
     // The parent path where the constructed index resides.
-    // HW1: n/a
-    // HW2/HW3: This is where the index is built into and loaded from.
     public String _indexPrefix = null;
-
+    
     // The specific Indexer to be used.
     public String _indexerType = null;
 
-    // The specific CorpusAnalyzer to be used.
-    public String _corpusAnalyzerType = null;
-
-    // The specific LogMiner to be used.
-    public String _logMinerType = null;
-
-    // Additional group specific configuration can be added below.
-    public int _iterations = 0;
-    public float _lambda = 0.0f;
-    public Integer _htmlDocNum= 0;
-    
- // The beta parameters for linear ranking model, used inside RankerLinear.
-    private static final String[] BETA_PARAMS = {
-      "beta_fav", "beta_pager", "beta_numviews"
-    };
-    public Map<String, Float> _betaValues;
-    /**
-     * Constructor for options.
-     * @param optionFile where all the options must reside
-     * @throws IOException
-     */
     public Options(String optionsFile) throws IOException {
       // Read options from the file.
       BufferedReader reader = new BufferedReader(new FileReader(optionsFile));
@@ -105,42 +41,21 @@ public class SearchEngine {
         options.put(vals[0].trim(), vals[1].trim());
       }
       reader.close();
-
-      // Populate global options.
-      _htmlDocNum = Integer.parseInt(options.get("html_doc_num"));
-      Check(_htmlDocNum != null, "Missing option: html_doc_num!");
       
+      // Populate global options.
       _corpusPrefix = options.get("corpus_prefix");
       Check(_corpusPrefix != null, "Missing option: corpus_prefix!");
-      _logPrefix = options.get("log_prefix");
-      Check(_logPrefix != null, "Missing option: log_prefix!");
       _indexPrefix = options.get("index_prefix");
       Check(_indexPrefix != null, "Missing option: index_prefix!");
-
+      
       // Populate specific options.
       _indexerType = options.get("indexer_type");
       Check(_indexerType != null, "Missing option: indexer_type!");
-
-      _corpusAnalyzerType = options.get("corpus_analyzer_type");
-      Check(_corpusAnalyzerType != null,"Missing option: corpus_analyzer_type!");
-
-      _logMinerType = options.get("log_miner_type");
-      Check(_logMinerType != null, "Missing option: log_miner_type!");
-      
-      _iterations = Integer.valueOf(options.get("iterations"));
-      Check(_iterations != 0, "Missing option: iterations!");
-      
-      _lambda = Float.valueOf(options.get("lambda"));
-      Check(_lambda != 0.0f, "Missing option: lambda!");
-      
-      _betaValues = new HashMap<String, Float>();
-      for (String s : BETA_PARAMS) {
-        _betaValues.put(s, Float.parseFloat(options.get(s)));
-      }
     }
   }
-
+  
   public static Options OPTIONS = null;
+
   /**
    * Prints {@code msg} and exits the program if {@code condition} is false.
    */
@@ -156,17 +71,15 @@ public class SearchEngine {
    */
   public static enum Mode {
     NONE,
-    MINING,
     INDEX,
     SERVE,
   };
+  
   public static Mode MODE = Mode.NONE;
 
   public static int PORT = -1;
 
-  private static void parseCommandLine(String[] args)
-      throws IOException, NumberFormatException {
-	  
+  /*private static void parseCommandLine(String[] args) throws IOException, NumberFormatException {
     for (String arg : args) {
       String[] vals = arg.split("=", 2);
       String key = vals[0].trim();
@@ -174,9 +87,7 @@ public class SearchEngine {
       if (key.equals("--mode") || key.equals("-mode")) {
         try {
           MODE = Mode.valueOf(value.toUpperCase());
-          
         } catch (IllegalArgumentException e) {
-        	System.out.println(e.getStackTrace());
           // Ignored, error msg will be printed below.
         }
       } else if (key.equals("--port") || key.equals("-port")) {
@@ -185,22 +96,15 @@ public class SearchEngine {
         OPTIONS = new Options(value);
       }
     }
-    Check(MODE == Mode.SERVE || MODE == Mode.INDEX || MODE == Mode.MINING,
-        "Must provide a valid mode: serve or index or mining!");
+    Check(MODE == Mode.SERVE || MODE == Mode.INDEX,
+        "Must provide a valid mode: serve or index!");
     Check(MODE != Mode.SERVE || PORT != -1,
         "Must provide a valid port number (258XX) in serve mode!");
     Check(OPTIONS != null, "Must provide options!");
   }
-
-  ///// Main functionalities start
-
-  private static void startMining()
-      throws IOException, NoSuchAlgorithmException {
-    
-    return;
-  }
   
-  private static void startIndexing() throws IOException, ClassNotFoundException {
+  ///// Main functionalities start
+  private static void startIndexing() throws IOException {
     Indexer indexer = Indexer.Factory.getIndexerByOption(SearchEngine.OPTIONS);
     Check(indexer != null,
         "Indexer " + SearchEngine.OPTIONS._indexerType + " not found!");
@@ -224,14 +128,29 @@ public class SearchEngine {
     System.out.println(
         "Listening on port: " + Integer.toString(SearchEngine.PORT));
   }
+  */
   
-  public static void main(String[] args) {
+  public Indexer indexer;
+  
+  public SearchEngine() throws ClassNotFoundException, IOException{
+	  OPTIONS = new Options("/Users/jinfanyang/Desktop/Project/Project/conf/engine.conf");
+	  if(indexer == null){
+		  indexer = Indexer.Factory.getIndexerByOption(SearchEngine.OPTIONS);
+		  Check(indexer != null, "Indexer " + SearchEngine.OPTIONS._indexerType + " not found!");
+		  File indexfile = new File(SearchEngine.OPTIONS._indexPrefix + "/corpus.idx");
+		  if(indexfile.exists()){
+			  indexer.loadIndex();
+		  }
+		  else{
+			  indexer.constructIndex();
+		  }  
+	  }
+  }
+  
+  /*public static void main(String[] args) {
     try {
       SearchEngine.parseCommandLine(args);
       switch (SearchEngine.MODE) {
-      case MINING:
-        startMining();
-        break;
       case INDEX:
         startIndexing();
         break;
@@ -242,9 +161,7 @@ public class SearchEngine {
         Check(false, "Wrong mode for SearchEngine!");
       }
     } catch (Exception e) {
-      System.err.println(e.getStackTrace());
       System.err.println(e.getMessage());
     }
-		System.out.println("Finished");
-	}
+  }*/
 }
